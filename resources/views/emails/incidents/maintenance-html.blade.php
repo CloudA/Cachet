bba9358a18c25c9c3d5c0f20f982c64de449826c
swat30 <jacobgodin@gmail.com>
2015-11-04 11:18:03 -0400

    Apply style changes to maintenance emails

--- resources/views/emails/incidents/maintenance-html.blade.php
+++ resources/views/emails/incidents/maintenance-html.blade.php
@@ -1,40 +1,20 @@
 @extends('layout.emails')
 
 @section('content')
-<h1 class="align-center">{!! $name !!}</h1>
+    <div style="text-align: center; border-bottom: 1px solid black; height: 100%;">
+        <h2>
+            <a href="http://status.clouda.ca/" style="color: #2a92e5;text-decoration: none;hover: initial;">{!! trans('cachet.subscriber.email.maintenance.html', ['app_name' => Setting::get('app_name')]) !!}</a>
+        </h2>
+    </div>
 
-<table class="border-rounded" width="100%" cellpadding="0" cellspacing="0">
-    <tr>
-        <td>
-            <p class="compressed">
-                <strong>{!! $status !!} @if($has_component) ({{ $component_name }}) @endif</strong>
-                {!! $html_content !!}
-                {!! $timestamp !!}
-            </p>
-        </td>
-    <tr>
-</table>
+    <h3>
+        {!! $status !!}
+    </h3>
 
-<table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0">
-    <tr>
-        <td align="center">
-            <div>
-                <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="{{ $manage_link }}" style="height:45px;v-text-anchor:middle;width:200px;" arcsize="7%" stroke="f" fill="t">
-                <v:fill type="tile" color="#22BC66" />
-                <w:anchorlock/>
-                <center style="color:#ffffff;font-family:sans-serif;font-size:15px;">{!! trans('cachet.subscriber.email.manage') !!}</center>
-                </v:roundrect><![endif]-->
-                <a href="{{ $manage_link }}" class="button button--green">{!! trans('cachet.subscriber.email.manage') !!}</a>
-            </div>
-        </td>
-    </tr>
-</table>
-
-<table class="body-sub" align="center">
-    <tr>
-        <td align="center">
-            <p class="sub"><a href="{{ $unsubscribe_link }}">{!! trans('cachet.subscriber.email.unsubscribe') !!}</a></p>
-        </td>
-    </tr>
-</table>
+    <p>
+        {!! $htmlContent !!}
+    </p>
+    <p>
+        <small><a href="{{ $unsubscribeLink }}">{!! trans('cachet.subscriber.email.unsubscribe') !!}</a></small>
+    </p>
 @stop
