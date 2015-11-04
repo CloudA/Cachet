6a51df9e06bfe531abe6784081cb6c0d2f2e9a63
swat30 <jacobgodin@gmail.com>
2015-11-04 10:48:17 -0400

    Copy+pasta problem

--- resources/views/emails/incidents/new-html.blade.php
+++ resources/views/emails/incidents/new-html.blade.php
@@ -5,13 +5,9 @@
 @stop
 
 @section('content')
-	<h2>
-		{!! trans('cachet.subscriber.email.incident.html-preheader', ['app_name' => Setting::get('app_name')]) !!}
-	</h2>
-
     <div style="text-align: center; border-bottom: 1px solid black; height: 100%;">
         <h2>
-            <a href="http://status.clouda.ca/" style="color: #2a92e5;text-decoration: none;hover: initial;">New incident has been reported on Cloud-A Status Page</a>
+            <a href="http://status.clouda.ca/" style="color: #2a92e5;text-decoration: none;hover: initial;">{!! trans('cachet.subscriber.email.incident.html-preheader', ['app_name' => Setting::get('app_name')]) !!}</a>
         </h2>
     </div>
 
