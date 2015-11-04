598a94d46d596a68180cec340238ca168abb52e2
swat30 <jacobgodin@gmail.com>
2015-11-04 11:02:06 -0400

    Lost our status

--- resources/views/emails/incidents/new-html.blade.php
+++ resources/views/emails/incidents/new-html.blade.php
@@ -7,20 +7,13 @@
         </h2>
     </div>
 
-<table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0">
-    <tr>
-        <td align="center">
-            <div>
-                <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="{{ $manage_link }}" style="height:45px;v-text-anchor:middle;width:200px;" arcsize="7%" stroke="f" fill="t">
-                <v:fill type="tile" color="#22BC66" />
-                <w:anchorlock/>
-                <center style="color:#ffffff;font-family:sans-serif;font-size:15px;">{!! trans('cachet.subscriber.email.manage') !!}</center>
-                </v:roundrect><![endif]-->
-                <a href="{{ $manage_link }}" class="button button--green">{!! trans('cachet.subscriber.email.manage') !!}</a>
-            </div>
-        </td>
-    </tr>
-</table>
+    <h3>
+        {!! $status !!}
+    </h3>
+
+    <p>
+        {!! $htmlContent !!}
+    </p>
 
 <table class="body-sub" align="center">
     <tr>
