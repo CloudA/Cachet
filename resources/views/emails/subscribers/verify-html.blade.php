7e85490978d4388a3f4449e710b594e7324e31eb
swat30 <jacobgodin@gmail.com>
2015-11-04 14:59:25 -0400

    Really bad at copy+paste today

--- resources/views/emails/subscribers/verify-html.blade.php
+++ resources/views/emails/subscribers/verify-html.blade.php
@@ -1,12 +1,6 @@
 @extends('layout.emails')
 
 @section('content')
-    <div style="text-align: center; border-bottom: 1px solid black; height: 100%;">
-        <h2>
-            <a href="http://status.clouda.ca/" style="color: #2a92e5;text-decoration: none;hover: initial;">{!! trans('cachet.subscriber.email.incident.html-preheader', ['app_name' => Setting::get('app_name')]) !!}</a>
-        </h2>
-    </div>
-
     <h3>
         Status Subscription
     </h3>
